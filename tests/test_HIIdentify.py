b01f65c1dcc3bfaa031e8093deff7a60160c5df2
Bethan Easeman <56442171+BethanEaseman@users.noreply.github.com>
2022-06-29 11:37:34 +0100

    Added: tests for more scenarios - all passing at the moment

--- tests/test_HIIdentify.py
+++ tests/test_HIIdentify.py
@@ -5,6 +5,7 @@ import unittest
 
 import numpy as np
 from astropy.io import fits
+import matplotlib.pyplot as plt
 
 import create_test_images as cti
 from HIIdentify.HIIdentify import identify_HII_regions
@@ -26,34 +27,15 @@ class HIIdentifyTestCase(unittest.TestCase):
 
     Checks that regions are correctly identified in test cases.
     """
-
-
-    # read in example images (setUp())
-
-    # For each of the example images: (?)
-
-    # Use HIIdentify to identify regions in the image
-    # Assert that regions found, and that conditions are met:
-    # - If bkg_flux used, check no pixels with flux <= bkg are selected
-    # - Check no peaks above flux_ulim are considered
-    # - Check stops at flux_llim
-    # - Check min_pixels condition is obeyed
-    # - Check how many peaks identified for each image / correct no. identified
-    # - Check that two regions are sensibly separated somehow if merged region used?
-    # - For noisy test image, check that noisy pixels properly dealt with
-    # - For noisy image with no peak, check this is dealt with correctly.
-
-
-
     def setUp(self):
         tdir = 'images_for_testing/'
         try:
             self.isr, self.isr_head = read_in_fits(f"{tdir}isolated_region.fits")
             self.nir, self.nir_head = read_in_fits(f"{tdir}noisy_isolated_region.fits")
-            self.fir,  self.fir_head = read_in_fits(f"{tdir}faint_isolated_region.fits")
-            self.mur, self.mur_head  = read_in_fits(f"{tdir}multiple_regions.fits")
-            self.mer, self.mer_head  = read_in_fits(f"{tdir}merged_regions.fits")
-            self.jun, self.jun_head  = read_in_fits(f"{tdir}just_noise.fits")
+            self.fir, self.fir_head = read_in_fits(f"{tdir}faint_isolated_region.fits")
+            self.mur, self.mur_head = read_in_fits(f"{tdir}multiple_regions.fits")
+            self.mer, self.mer_head = read_in_fits(f"{tdir}merged_regions.fits")
+            self.jun, self.jun_head = read_in_fits(f"{tdir}just_noise.fits")
         except FileNotFoundError:
             cti.isolated_region()
             cti.noisy_isolated_region()
@@ -69,22 +51,25 @@ class HIIdentifyTestCase(unittest.TestCase):
         Test for an isolated region.
         """
 
-        ideal_seg_map = np.array(self.isr[self.isr>0.5], dtype=int)
-
-        identify_HII_regions(self.isr, self.isr_head,flux_llim=1, #pylint: disable=invalid-name
+        identify_HII_regions(self.isr.copy(), self.isr_head, flux_llim=1, #pylint: disable=invalid-name
                              flux_ulim=20, z=0.001, obj_name='isolated_region', bkg_flux=0.5,
-                             max_radius_kpc=5, max_radius_arcsec=None,
+                             max_radius_kpc=.5, max_radius_arcsec=None,
                              min_pixels=5, verbose=False, tdir='test_results/')
 
-        ideal_map = np.array(self.isr > 0.5, dtype=int)
+        ideal_map = np.zeros_like(self.isr)
+        ideal_map[self.isr > 0.5] = 1
 
         with fits.open('test_results/isolated_region_HII_segmentation_map.fits') as tmp:
-            seg_map = tmp['Region_IDs'].data
+            seg_map = tmp['Region_IDs'].data  # pylint: disable=no-member
+
+        #Check that only one region identified
+        self.assertTrue(np.nanmax(seg_map) == 1)
 
         #Check that > 90% of the pixels identified in the real map match the ideal map:
         num_matching = np.count_nonzero(ideal_map == seg_map)
-        ideal_total = np.count_nonzero(ideal_map)
-        seg_total = np.count_nonzero(seg_map)
+        ideal_total = np.count_nonzero(self.isr > 0.5)
+        seg_total = np.count_nonzero((seg_map > 0))
+
 
         self.assertTrue(num_matching > (0.9 * ideal_total))
         self.assertTrue(num_matching > (0.9 * seg_total))
@@ -94,8 +79,96 @@ class HIIdentifyTestCase(unittest.TestCase):
 
 
 
+    def test_noisy_isolated_region(self):
+        """
+        Test for a noisy isolated region
+        """
+
+        identify_HII_regions(self.nir.copy(), self.nir_head, flux_llim=9, # pylint: disable=invalid-name
+                             flux_ulim=20, z=0.001, obj_name='noisy_isolated_region', bkg_flux=0.5,
+                             max_radius_kpc=.5, max_radius_arcsec=None,
+                             min_pixels=5, min_distance=.1, verbose=False, tdir='test_results/')
+
+        with fits.open('test_results/noisy_isolated_region_HII_segmentation_map.fits') as tmp:
+            seg_map = tmp['Region_IDs'].data  # pylint: disable=no-member
+
+        ideal_map = np.zeros_like(self.nir)
+        ideal_map[self.nir > 0.5] = 1
+
+        #Check that only one region identified
+        self.assertTrue(np.nanmax(seg_map) == 1)
+
+        num_matching = np.count_nonzero(ideal_map == seg_map)
+        ideal_total = np.count_nonzero(self.nir > 0.5)
+        seg_total = np.count_nonzero((seg_map > 0))
+
+        print(f"Noisy isolated region: {ideal_total} pixels identified in the ideal map, \
+{seg_total} in the seg map")
+
+        #Check that the seg map doesn't identify more pixels as belonging to the region than the
+        # ideal map
+        self.assertTrue(seg_total < ideal_total)
+
+        #Check that > 50% of the pixels identified in the real map match the ideal map:
+        self.assertTrue(num_matching > (0.5 * ideal_total))
 
 
+
+    def test_just_noise(self):
+        """
+        Test that no regions are identified in the just_noise map
+        """
+
+        identify_HII_regions(self.jun.copy(), self.jun_head, flux_llim=9, # pylint: disable=invalid-name
+                             flux_ulim=20, z=0.001, obj_name='just_noise', bkg_flux=5,
+                             max_radius_kpc=.5, max_radius_arcsec=None,
+                             min_pixels=5, min_distance=.1, verbose=False, tdir='test_results/')
+
+
+        with fits.open('test_results/just_noise_HII_segmentation_map.fits') as tmp:
+            seg_map = tmp['Region_IDs'].data  # pylint: disable=no-member
+
+
+        #Check that no regions identified
+        self.assertTrue(np.nanmax(seg_map) == -50)
+
+
+
+
+    def test_multiple_regions(self):
+        """
+        Test for multiple isolated regions
+        """
+
+        identify_HII_regions(self.mur.copy(), self.mur_head, flux_llim=9, # pylint: disable=invalid-name
+                             flux_ulim=20, z=0.001, obj_name='multiple_regions', bkg_flux=0.5,
+                             max_radius_kpc=.5, max_radius_arcsec=None,
+                             min_pixels=5, min_distance=.1, verbose=False, tdir='test_results/')
+
+
+        ideal_map = np.zeros_like(self.mur)
+        ideal_map[self.mur > 0.5] = 1
+
+        with fits.open('test_results/multiple_regions_HII_segmentation_map.fits') as tmp:
+            seg_map = tmp['Region_IDs'].data  # pylint: disable=no-member
+
+
+        #Check that only two regions identified
+        self.assertTrue(np.nanmax(seg_map) == 2)
+
+        num_identified = np.count_nonzero((ideal_map > 0) & (seg_map > 0))
+        ideal_total = np.count_nonzero(self.mur > 0.5)
+        seg_total = np.count_nonzero((seg_map > 0))
+
+        print(f"Multiple regions: {ideal_total} pixels identified in the ideal map, {seg_total} \
+in the seg map")
+
+        #Check that the seg map doesn't identify more pixels as belonging to the region than the
+        # ideal map
+        self.assertTrue(seg_total < ideal_total)
+
+        #Check that > 80% of the pixels identified in the real map match the ideal map:
+        self.assertTrue(num_identified > (0.8 * ideal_total))
 
 
 
