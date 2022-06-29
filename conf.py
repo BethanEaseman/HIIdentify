312948a2e3f5858d6924b9e6294249247aec7181
Bethan Easeman <56442171+BethanEaseman@users.noreply.github.com>
2022-06-29 13:29:54 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,12 +2,11 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HIIdentify'
+author = 'Bethan Easeman'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0.1'
+version = '0.0.1'
 
 # -- General configuration
 
@@ -17,6 +16,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'myst_parser'
 ]
 
 intersphinx_mapping = {
